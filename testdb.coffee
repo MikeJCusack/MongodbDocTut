5401ed273937fe963236491d71a116e3cb2446ac
Michael Joseph Cusack <whoneedszzz@gmail.com>
2016-02-04 11:12:17 -0600

    Insert a document into the test db (insertOne())

--- testdb.coffee
+++ testdb.coffee
@@ -1,9 +1,38 @@
 # out: testdb.js
 MongoClient = require('mongodb').MongoClient
 assert = require 'assert'
+ObjectId = require('mongodb').ObjectID
 url = 'mongodb://localhost:27017/test'
+
+insertDoc = (db, callback) ->
+  db.collection('restaurants').insertOne {
+    "address":
+      "street": "2 Avenue"
+      "zipcode": "10075"
+      "building": "1480"
+      "coord": [ -73.9557413, 40.7720266 ]
+    "borough": "Manhattan"
+    "cuisine": "Italian"
+    "grades": [
+      {
+        "date": new Date "2014-10-01T00:00:00Z"
+        "grade": "A"
+        "score": 11
+      }, {
+        "date": new Date "2014-01-16T00:00:00Z"
+        "grade": "B"
+        "score": 17
+      }
+    ]
+    "name": "Vella"
+    "restaurant_id": "41704620"
+  }, (err, result) ->
+    assert.equal null, err
+    console.log "Inserted a document into the restaurants collection."
+    callback result
 
 MongoClient.connect url, (err, db) ->
   assert.equal null, err
-  console.log "Connected successfully to server."
-  db.close()
+  insertDoc db, ->
+    db.close()
+  console.log "All operations completed successfully."
--- testdb.js
+++ testdb.js
@@ -1,16 +1,50 @@
 (function() {
-  var MongoClient, assert, url;
+  var MongoClient, ObjectId, assert, insertDoc, url;
 
   MongoClient = require('mongodb').MongoClient;
 
   assert = require('assert');
 
+  ObjectId = require('mongodb').ObjectID;
+
   url = 'mongodb://localhost:27017/test';
+
+  insertDoc = function(db, callback) {
+    return db.collection('restaurants').insertOne({
+      "address": {
+        "street": "2 Avenue",
+        "zipcode": "10075",
+        "building": "1480",
+        "coord": [-73.9557413, 40.7720266]
+      },
+      "borough": "Manhattan",
+      "cuisine": "Italian",
+      "grades": [
+        {
+          "date": new Date("2014-10-01T00:00:00Z"),
+          "grade": "A",
+          "score": 11
+        }, {
+          "date": new Date("2014-01-16T00:00:00Z"),
+          "grade": "B",
+          "score": 17
+        }
+      ],
+      "name": "Vella",
+      "restaurant_id": "41704620"
+    }, function(err, result) {
+      assert.equal(null, err);
+      console.log("Inserted a document into the restaurants collection.");
+      return callback(result);
+    });
+  };
 
   MongoClient.connect(url, function(err, db) {
     assert.equal(null, err);
-    console.log("Connected successfully to server.");
-    return db.close();
+    insertDoc(db, function() {
+      return db.close();
+    });
+    return console.log("All operations completed successfully.");
   });
 
 }).call(this);
